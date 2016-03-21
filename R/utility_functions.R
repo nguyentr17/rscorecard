f03775d199e8e0ca747ca46ef0b95c0f1e3d4325
btskinner <b.skinner@vanderbilt.edu>
2016-03-21 18:23:53 -0500

    added dictionary

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -3,6 +3,12 @@
 magrittr::`%>%`
 
 #' @importFrom jsonlite fromJSON
+#' @importFrom dplyr filter_
+#' @export
+dplyr::filter_
+#' @importFrom lazyeval interp
+#' @export
+lazyeval::interp
 
 ## paste pipes
 `%+%`  <- function(a,b) paste(a, b, sep = '')
