489d176256d32e9c43bafe4c0311bf1383d44352
Benjamin Skinner <btskinner@virginia.edu>
2018-09-06 23:27:06 -0400

    added support for some tidyselect helper functions

--- R/select.R
+++ R/select.R
@@ -56,6 +56,37 @@ sc_select_ <- function(sccall, vars) {
              call. = FALSE)
     }
 
+    ## look for tidyselect helpers
+    shl <- c('starts_with','ends_with','contains','matches','everything')
+    col <- ifelse(sccall[['dfvars']], 'dev_friendly_name', 'varname')
+    for (i in 1:length(vars)) {
+        v <- vars[[i]]
+        if (is.call(v)) {
+            v <- as.list(parse(text = gsub('^_', 'zzz_', v))) # no leading hyphens
+            v[[1]] <- as.character(v[[1]])
+            if (v[[1]] != 'everything') {
+                v[[2]] <- gsub('^zzz_', '_', as.character(v[[2]]))
+            }
+            if (v[[1]] %in% shl) {
+                if (v[[1]] != 'everything') {
+                    fun <- get(v[[1]], asNamespace('tidyselect'))
+                    index <- do.call(fun, list(v[[2]], vars = dict[[col]]))
+                    v <- dict[index, col]
+                } else {
+                    v <- unique(dict[[col]])
+                }
+            } else {
+                stop('Can only use the following tidyselect helpers:\n',
+                     paste(' - ', paste0(shl, '()'), collapse='\n'),
+                     call. = FALSE)
+            }
+        }
+        vars[[i]] <- v
+    }
+
+    ## unlist vars
+    vars <- unlist(vars, use.names = FALSE)
+
     ## confirm variables exist in dictionary
     for (v in vars) {
         if (!sc_dict(tolower(as.character(v)), confirm = TRUE)) {
